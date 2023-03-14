00d930e2122d0d81d444f1e0521900dc6ed35407
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-03-14 17:56:04 -0600

    debugged contracts

--- staking_dapp/src/contracts/dummy_token.sol
+++ staking_dapp/src/contracts/dummy_token.sol
@@ -1,32 +1,33 @@
+// SPDX-License-Identifier: MIT
 pragma solidity  >= 0.4.22 <0.9.0;
 
-contract Tether {
+contract dummy {
 
     string public name = "dummy_token";
     string public symbol = "dummy_token";
-    unit public totalsupply = 100000000000000000;
-    unit public decimal = 18;
+    uint public totalsupply = 100000000000000000;
+    uint public decimal = 18;
 
-    event Transfer(
+    event transfer(
         address indexed _from,
         address indexed _to,
-        unit _value
+        uint _value
     );
 
     event approve(
         address indexed _owner,
         address indexed _spender,
-        unit _value
+        uint _value
     );
 
-    mapping(address => unit256) public balance;
-    mapping(address => mapping(address=> unit256)) public allowance;
+    mapping(address => uint256) public balance;
+    mapping(address => mapping(address=> uint256)) public allowance;
 
-    constructor() public {
+    constructor() {
         balance [msg.sender] = totalsupply;
     }
 
-    function transfer(address _to, unit256 _value) public returns(bool success){
+    function transferIt(address _to, uint256 _value) public returns(bool success){
         require(balance[msg.sender] >= _value);
         balance[msg.sender] -= _value;
         balance[_to] += _value;
@@ -34,15 +35,15 @@ contract Tether {
         return true;
     }
 
-    function approve(address _spender, unit256 _value) public returns(bool success) {
+    function approveIt(address _spender, uint256 _value) public returns(bool success) {
         allowance[msg.sender][_spender] = _value;
         emit approve(msg.sender, _spender, _value);
         return true;
     }
 
-    function transerfrom(address _from, address _to, unin256 _value) public returns(bool success) {
+    function transerfrom(address _from, address _to, uint256 _value) public returns(bool success) {
         require(_value <= balance[_from]);
-        require(_valeu <= allowance[_from][msg.sender]);
+        require(_value <= allowance[_from][msg.sender]);
         balance[_from] -= _value;
         balance[_to] += _value;
         allowance[_from][msg.sender] -= _value;
--- staking_dapp/src/contracts/staking_dapp.sol
+++ staking_dapp/src/contracts/staking_dapp.sol
@@ -1,6 +1,7 @@
+// SPDX-License-Identifier: MIT
 pragma solidity  >= 0.4.22 <0.9.0;
 import "./dummy_token.sol";
-import "./tether.sol";
+import "./tether_token.sol";
 
 contract staking_dapp{
 
@@ -14,13 +15,13 @@ contract staking_dapp{
     mapping(address => bool) public hasStaked;
     mapping(address => bool) public isStaked;
 
-    constructor (dummy _dummytoken, Tether tethertoken) public {
+    constructor (dummy _dummytoken, Tether _tethertoken) public {
         dummy_token = _dummytoken;
         tether_token = _tethertoken;
         owner = msg.sender;
     }
 
-    function stakeToken(unit _amount) public {
+    function stakeToken(uint _amount) public {
         require(_amount > 0, "amount cannot be zero");
         tether_token.transferfrom(msg.sender, address(this), _amount);
         stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
@@ -38,8 +39,8 @@ contract staking_dapp{
         uint balance = stakingBalance[msg.sender];
         require(balance > 0, "staking balance is zero");
         tether_token.transfer(msg.sender, balance);
-        stakingbalance[msg.sender] = 0;
-        isstaking[msg.sender] = false;
+        stakingBalance[msg.sender] = 0;
+        isStaking[msg.sender] = false;
     }
 
     function issueDummy() public {
@@ -47,7 +48,7 @@ contract staking_dapp{
         require(msg.sender == owner, "caller must be the owner");
         for(uint i=0; i<stakers.length; i++) {
             address recipient = stakers[i];
-            uint balance = stakingbalance[recipient];
+            uint balance = stakingBalance[recipient];
             if(balance > 0) {
                 dummy_token.transfer(recipient, balance);
             }
